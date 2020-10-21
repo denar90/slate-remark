7e75bcfc2dbbba143184f721be3fefd46c051eb7
Artem Denysov <denysov.artem@gmail.com>
2020-10-21 13:41:03 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as remarkToSlate } from "./plugins/remark-to-slate";
 export { default as slateToRemark } from "./plugins/slate-to-remark";
 export { default as slateToRemarkLegacy } from "./plugins/slate0.47-to-remark";
 export { default as remarkToSlateLegacy } from "./plugins/remark-to-slate0.47";
-export { default as slateLegacyToSlate } from "./plugins/slate0.47-to-slate.ts";
+export { default as slateLegacyToSlate } from "./plugins/slate0.47-to-slate";
